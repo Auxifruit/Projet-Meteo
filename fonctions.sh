39a68c6d8044ad7ba09c4afeecf745b4b41e1428
Auxifruit <105287640+Auxifruit@users.noreply.github.com>
2023-01-14 15:29:09 +0100

    Update fonctions.sh

--- fonctions.sh
+++ fonctions.sh
@@ -10,17 +10,21 @@ Tri_en_C () {
     if [ "$2" -eq 1 ] ; then
         if [ -n "$4" ] ; then
             echo "$4"
+            # r = 1
         fi
         echo "Fonction tri ABR."
+        # fonction_abr($1,r)
     elif [ $3 -eq 1 ] ; then
         if [ -n "$4" ] ; then
             echo "$4"
         fi
         echo "Fonction tri liste chainée."
+        # fonction_tab($1,r)
     else
         if [ -n "$4" ] ; then
             echo "$4"
         fi
         echo "Fonction tri AVL."
+        # fonction_avl($1,r)
     fi
 }
