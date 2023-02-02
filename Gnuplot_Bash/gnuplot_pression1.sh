92f1c646e99332701a5920dddb60f15d96b6e237
Auxifruit <105287640+Auxifruit@users.noreply.github.com>
2023-02-02 19:25:17 +0100

    Update gnuplot_pression1.sh

--- Gnuplot_Bash/gnuplot_pression1.sh
+++ Gnuplot_Bash/gnuplot_pression1.sh
@@ -4,5 +4,6 @@ gnuplot -persist <<-EOFMarker
     set title "Pression des stations"
     set xlabel "ID de station"
     set ylabel "Pression"
-    plot 'pression1_trie.txt' using log(1):3:2 with filledcurve title "Marge d'erreur" lc rgb '#fde725', '' using log(1):4 smooth mcspline lw 2 title "Pression moyenne"
+    set xtics rotate
+    plot 'pression1_trie.txt' using log(1):3:2:xtic(1) with filledcurve title "Ecart avec min et max" lc rgb '#fde725', '' using log(1):4 smooth mcspline lw 2 title "Pression moyenne"
 EOFMarker
