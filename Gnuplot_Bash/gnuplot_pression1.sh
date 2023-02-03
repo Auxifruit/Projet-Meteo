cd9ef1989645bb3413afef346784752d19b528b8
Auxifruit <105287640+Auxifruit@users.noreply.github.com>
2023-02-03 23:57:46 +0100

    Delete Gnuplot_Bash directory

--- Gnuplot_Bash/gnuplot_pression1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-gnuplot -persist <<-EOFMarker
-    set title "Pression des stations"
-    set xlabel "ID de station"
-    set ylabel "Pression"
-    set xtics rotate
-    plot 'pression1_trie.txt' using log(1):3:2:xtic(1) with filledcurve title "Ecart avec min et max" lc rgb '#fde725', '' using log(1):4 smooth mcspline lw 2 title "Pression moyenne"
-EOFMarker
--- Gnuplot_Bash/gnuplot_temperature1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-gnuplot -persist <<-EOFMarker
-    set title "Température des stations"
-    set xlabel "ID de station"
-    set ylabel "Température"
-    set xtics rotate 
-    plot 'temperature1_trie.txt' using log(1):3:2:xtic(1) with filledcurve title "Ecart avec min et max" lc rgb '#fde725', '' using log(1):4 smooth mcspline lw 2 title "Température moyenne"
-EOFMarker
